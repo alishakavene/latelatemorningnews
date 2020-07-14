bd3f2b8c3ee45ac2a7983c65a68ea4a85fd61525
alishakavene <akmunywoki@gmail.com>
2020-07-14 17:04:24 +0300

    modify files

--- app/news.py
+++ app/news.py
@@ -1,12 +1,27 @@
+
 class News:
 
-    def __init__(self,id,name,description,url,category,language,country):
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
     
         self.id = id
         self.name = name
+        self.author = author
+        self.title = title
         self.description = description
         self.url  = url
-        self.category = category
-        self.language = language
-        self.country = country
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+        
 
+class Headline:
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
+         self.id = id
+         self.name = name
+         self.author = author
+         self.title = title
+         self.description = description
+         self.url = url
+         self.urlToImage = urlToImage
+         self.publishAt = publishedAt
+         self.content = content
--- app/request.py
+++ app/request.py
@@ -1,15 +1,19 @@
-from app import app
+
 import urllib.request,json
 from app.news import News
 
-
-
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
-print(api_key)
-#Getting News base url
-base_url = app.config["NEWS_API_SOURCE_URL"]
-print(base_url)
+api_key = None
+# Getting the movie base url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    print(api_key)
+    base_url = app.config["NEWS_API_SOURCE_URL"]
+    print (base_url)
+
 
 
 
@@ -23,8 +27,9 @@ def get_news():
 
         news_sources = None
         
-        if get_news_response['sources']:
-            news_sources_list = get_news_response['sources']
+        if get_news_response['articles']:
+
+            news_sources_list = get_news_response['articles']
             news_sources = process_sources(news_sources_list)
            
            
@@ -38,20 +43,43 @@ def process_sources(news_list):
 
     news_sources = []
     for news_item in news_list:
-
-        id = news_item['id']
-        name = news_item['name']
+    
+        id = news_item['source']['id']
+        name = news_item['source']['name']
+        title = news_item['title']
+        author = news_item['author']
         description = news_item['description']
         url = news_item['url']
-        category = news_item['category']
-        language = news_item['language']
-        country = news_item['country']
+        urlToImage = news_item ['urlToImage']
+        publishedAt = news_item ['publishedAt']
+        content = news_item['content']
 
         if url:
-            news_object = News(id,name,description,url,category,language,country)
+            news_object = News(id,name,title,author,description,url,urlToImage,publishedAt,content)
             news_sources.append(news_object)
     
     return news_sources
+
+def get_headline():
+    get_headline_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_headline_details_url) as url:
+        headline_details_data = url.read()
+        headline_details_response = json.loads(headline_details_data)
+        headline_object = None
+        if headline_details_response:
+             id = news_item('id')
+             name = news_item('name')
+             description = news_item('description')
+             url = news_item('url')
+             category = news_item('category')
+             language = news_item('language')
+             country = news_item('country')
+
+             headline_object = Headline(id.name,description,url,category,language,country)
+
+        return headline_object
+
 
 
 
